2c93a0c39f763b8809a36c4b9a29e151141ae266
accorrales <andercorrales28@gmail.com>
2026-06-04 14:18:03 -0600

    agrega panel admin compras pendiente_1s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
-/*export const environment = {
+export const environment = {
   production: false,
   apiUrl: 'https://zairoofficial-production.up.railway.app/api'
 };
-*/
-export const environment = {
+
+/*export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api'
-};
+}; */
